9f637fa2ad49d5fb2935d653a8c8eb23e48d6873
Hannes Weisbach <hannes.weisbach@tu-dresden.de>
2015-03-23 17:32:42 +0100

    Convert atlas_debug syscall to debugfs
    Part 1: convert debugging of runqueues to debugfs

--- kernel/sched/atlas_common.h
+++ kernel/sched/atlas_common.h
@@ -17,11 +17,70 @@ enum debug {
 	SWITCH_SCHED,
 	ADAPT_SEXEC,
 	SLACK_TIME,
-  NUM_FLAGS
+	NUM_FLAGS
 };
 
 u32 is_flag_enabled(enum debug);
 void init_atlas_debugfs(void);
+
+static inline struct atlas_job *
+pick_first_job(const struct atlas_rq const *atlas_rq)
+{
+	struct rb_node *first = rb_first(&atlas_rq->jobs);
+
+	if (!first)
+		return NULL;
+
+	return rb_entry(first, struct atlas_job, rb_node);
+}
+
+static inline struct atlas_job *pick_next_job(const struct atlas_job const *job)
+{
+	struct rb_node *next = rb_next(&job->rb_node);
+
+	if (!next)
+		return NULL;
+
+	return rb_entry(next, struct atlas_job, rb_node);
+}
+
+static inline struct sched_atlas_entity *
+pick_first_entity(const struct atlas_rq const *atlas_rq)
+{
+	struct rb_node *left = atlas_rq->rb_leftmost_se;
+
+	if (!left)
+		return NULL;
+
+	return rb_entry(left, struct sched_atlas_entity, run_node);
+}
+
+static inline struct sched_atlas_entity *
+pick_next_entity(const struct sched_atlas_entity const *se)
+{
+	struct rb_node *next = rb_next(&se->run_node);
+
+	if (!next)
+		return NULL;
+
+	return rb_entry(next, struct sched_atlas_entity, run_node);
+}
+
+static inline ktime_t job_start(const struct atlas_job const *s)
+{
+	return ktime_sub(s->sdeadline, s->sexectime);
+}
+
+static inline int ktime_zero(const ktime_t a)
+{
+	return ktime_equal(ktime_set(0, 0), a);
+}
+
+static inline struct task_struct *
+task_of(const struct sched_atlas_entity const *se)
+{
+	return container_of(se, struct task_struct, atlas);
+}
 
 #define atlas_debug(flag, fmt, ...)                                            \
 	do {                                                                   \
