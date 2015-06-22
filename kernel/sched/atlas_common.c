b5db5fef892e133bab8351021d82693aefdef2c1
Hannes Weisbach <hannes.weisbach@tu-dresden.de>
2015-06-22 17:04:34 +0200

    Add nr_running for all RQs to RQ debug header

--- kernel/sched/atlas_common.c
+++ kernel/sched/atlas_common.c
@@ -96,14 +96,16 @@ size_t print_timeline(const struct atlas_job_tree *tree, char *buf,
 	const struct atlas_job *job;
 	struct rq *rq = tree->rq;
 
-	offset += scnprintf(
-			&buf[offset], size - offset,
-			"%s %d (%d/%d/%d/%d) (%d)%s:\n", tree->name, cpu_of(rq),
-			rq->nr_running, rq->atlas.atlas_jobs.nr_running,
-			rq->atlas.recover_jobs.nr_running,
-			rq->atlas.cfs_jobs.nr_running, rq_nr_jobs(&tree->jobs),
-			(rq->atlas.timer_target == ATLAS_SLACK) ? " (slack)"
-								: "");
+	offset += scnprintf(&buf[offset], size - offset,
+			    "%s %d (%d %u/%lu %d/%d/%d %d) (%d)%s:\n",
+			    tree->name, cpu_of(rq), rq->nr_running,
+			    rq->rt.rt_nr_running, rq->dl.dl_nr_running,
+			    rq->atlas.atlas_jobs.nr_running,
+			    rq->atlas.recover_jobs.nr_running,
+			    rq->atlas.cfs_jobs.nr_running, rq->cfs.nr_running,
+			    rq_nr_jobs(&tree->jobs),
+			    (rq->atlas.timer_target == ATLAS_SLACK) ? " (slack)"
+								    : "");
 
 	for (job = pick_first_job(tree); job; job = pick_next_job(job)) {
 		offset += print_atlas_job(job, &buf[offset], size - offset);
