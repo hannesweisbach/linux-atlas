11acd77743f35bebf1f1ae2ecdfd6feed233ab1c
Hannes Weisbach <hannes.weisbach@tu-dresden.de>
2015-08-12 18:08:25 +0200

    Add sysctl variable controlling load balancing in select_task_rq

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -44,6 +44,7 @@ extern unsigned int sysctl_sched_child_runs_first;
 extern unsigned int sysctl_sched_atlas_min_slack;
 extern unsigned int sysctl_sched_atlas_advance_in_cfs;
 extern unsigned int sysctl_sched_atlas_idle_job_stealing;
+extern unsigned int sysctl_sched_atlas_wakeup_balancing;
 #endif
 
 enum sched_tunable_scaling {
